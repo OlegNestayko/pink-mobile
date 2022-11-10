5b6ed2090d33dcd4be5302ce317dc2309c6b9c95
OlegNestayko <oleg_nestayko@ukr.net>
2022-11-10 18:53:35 +0200

    updated section "hero".

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $('.header-burger').click(function (e) {
     $('.header-burger,.header-menu').toggleClass('active');
+    $('body').toggleClass('lock');
   });
 });
