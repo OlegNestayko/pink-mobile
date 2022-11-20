35b85d069b64e08932cd4dd7cab9804b68f77df5
OlegNestayko <oleg_nestayko@ukr.net>
2022-11-20 22:48:00 +0200

    Update slider section.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+// Burger menu
 $(document).ready(function () {
   $('.header-burger').click(function (e) {
     $('.header-burger,.header-menu').toggleClass('active');
     $('body').toggleClass('lock');
+  });
+});
+
+// Slick slider
+$(document).ready(function () {
+  $('.slider-slick').slick({
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
   });
 });
